bd57ddc87ee7beb29071183e56f5318ed8acc6f7
SalHe <SalHe@qq.com>
2022-03-19 22:44:35 +0800

    init

---
+++ update-images.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker pull registry.cn-hangzhou.aliyuncs.com/salhe-general/se2022-qiaqia-back-end:nightly
+docker pull registry.cn-hangzhou.aliyuncs.com/salhe-general/se2022-qiaqia-front-end:nightly
